9c3baca6fd4e85c366b83ef5ddac166bb8fc003f
HristiyanG <Hristiyan@limechain.tech>
2020-11-09 17:24:46 +0200

    implemented user roles

---
+++ src/api/middlewares/admin-auth.js
@@ -0,0 +1,38 @@
+//@ts-nocheck
+const APIError = require('./../api-error');
+const AuthService = require('../services/auth-service')
+const mongooseService = require('../../database/index.js');
+const roles = require('../../database/User/user-roles')
+
+class AdminAuth {
+
+    static async validateAdminAccess(req, res, next) {
+
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+
+        if (token == null) {
+            return next(new APIError(401, 'Unauthorized.'))
+        }
+
+        try {
+            const userObj = await AuthService.verifyToken(token)
+            const ethAddress = userObj.user.toLowerCase()
+            const user = await mongooseService.getUser(ethAddress)
+            
+            if (user.role != roles.ADMIN) {
+                return next(new APIError(403, 'User is not admin!'));
+            }
+
+            res.locals.address = ethAddress;
+
+        } catch (error) {
+            console.log(error);
+            return next(new APIError(403, 'Forbidden.'))
+        }
+
+        next();
+    }
+}
+
+module.exports = AdminAuth
--- src/database/User/user.js
+++ src/database/User/user.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
 const User = require('../models/User')
-const Voucher = require('../models/Voucher')
+const userRoles = require('./user-roles');
 
 class UserService {
 
@@ -14,11 +14,42 @@ class UserService {
         return user.nonce;
     }
 
+    static async getUser(address) {
+        return await User.findOne({ address })
+    }
+
+    static async createUser(address, nonce) {
+        const user = new User({
+            address,
+            nonce,
+            role: userRoles.USER
+        })
+
+        await user.save();
+    }
+
+    static async setUserToAdmin(address) {
+        return await User.findOneAndUpdate(
+            { address: address },
+            { role: userRoles.ADMIN},
+            { new: true, upsert: true }
+        )
+    }
+
     static async preserveNonce(address, nonce) {
+
+        const user = await User.findOne({ address: address })
+
+        if (!user) {
+            return await UserService.createUser(address, nonce); 
+        }
 
         await User.findOneAndUpdate(
             { address: address},
-            { address, nonce },
+            { 
+                address, 
+                nonce,
+            },
             { new: true, upsert: true }
         )
     }
