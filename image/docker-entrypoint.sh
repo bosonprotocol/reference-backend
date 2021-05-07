63762fb7f39eb57958b04d129fa5a0bc5b04e3b0
Toby Clemson <tobyclemson@gmail.com>
2021-05-07 11:10:07 +0100

    Add debugging in docker entrypoint.

--- image/docker-entrypoint.sh
+++ image/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+set -o pipefail
+
 # Ensure AWS_S3_BUCKET_REGION environment variable is present
 if [ -z "$AWS_S3_BUCKET_REGION" ]; then
   echo >&2 'Error: missing AWS_S3_BUCKET_REGION environment variable.'
@@ -11,6 +14,11 @@ if [ -z "$AWS_S3_ENV_FILE_OBJECT_PATH" ]; then
   echo >&2 'Error: missing AWS_S3_ENV_FILE_OBJECT_PATH environment variable.'
   exit 1
 fi
+
+echo "$PATH"
+ls -la /usr/local
+ls -la /usr/local/bin
+ls -la /usr/local/aws
 
 # Fetch and source env file
 eval $(aws s3 cp \
