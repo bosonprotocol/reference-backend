7678482a96cea20ec775ec101b3eb3da7105834a
Radu Stefanache <radustefanache@redeemeum.com>
2021-12-22 14:45:36 +0000

    [ADD] Github Actions

--- infra/database/project.tf
+++
@@ -1,9 +0,0 @@
-module "project" {
-  source  = "infrablocks/project/mongodbatlas"
-  version = "1.0.0"
-
-  component = var.component
-  deployment_identifier = var.deployment_identifier
-
-  organization_id = var.mongodb_atlas_organization_id
-}
--- infra/functions/modules/lambda/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  base_tags = {
-    "Component" = var.component,
-    "DeploymentIdentifier" = var.deployment_identifier
-  }
-
-  tags = merge(var.tags, local.base_tags)
-}
